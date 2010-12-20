51997301384f3d4a1eea760c27bad753e0f8c5f6
Bill Pikounis <cg@billpikounis.net>
2010-12-20 00:00:00 +0000

    version 0.9.0

---
+++ R/00virtual.R
@@ -0,0 +1,34 @@
+## $Id: 00virtual.R 143 2010-09-06 01:58:42Z user $ 
+
+## Virtual Classes for cg package
+## For Polymorphic slots
+
+setOldClass("lm")
+setOldClass("rlm")
+setOldClass("survreg")
+setOldClass("gls")
+
+setClass("olsfit")
+setClass("rrfit")
+setClass("aftfit")
+setClass("uvfit")
+
+setClassUnion("olsfit", c("character", "lm"))
+setClassUnion("rrfit", c("character", "rlm", "lm"))
+setClassUnion("aftfit", c("character", "survreg"))
+setClassUnion("uvfit", c("character", "gls"))
+
+setClass("numericOrNULL")
+setClassUnion("numericOrNULL", c("numeric", "NULL"))
+
+setClass("dataframeMatrixOrNULL")
+setClassUnion("dataframeMatrixOrNULL", c("data.frame", "matrix", "NULL"))
+
+setClass("dataframeOrNULL")
+setClassUnion("dataframeOrNULL", c("data.frame", "NULL"))
+
+setClass("characterOrNULL")
+setClassUnion("characterOrNULL", c("character", "NULL"))
+
+setClass("characterOrExpression")
+setClassUnion("characterOrExpression", c("character", "expression"))
